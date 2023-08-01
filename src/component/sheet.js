699fdf42ec3fad80eaf2f50659b23bf600834b01
Xiujun Ma <xma@compusoftdevelopment.com>
2023-08-01 14:03:00 -0600

    [DEV-4595] Formulas are formatted as General (This is a seriously bad bug)

--- src/component/sheet.js
+++ src/component/sheet.js
@@ -636,6 +636,10 @@ function dataSetCellTextSelector(selector, text, type, state = 'finished') {
                 decimal: 0
             });
             newCell.style = data.styles.length - 1;
+        } else if (newCell && data.styles[newCell.style] && data.styles[newCell.style].format !== 'number') {
+            const existingStyle = Object.assign({}, data.styles[newCell.style]);
+            existingStyle.format = 'number';
+            newCell.style = data.styles.push(existingStyle) - 1;
         }
     }
 
